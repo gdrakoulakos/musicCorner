ad8f50b6c558118cc2117da3f45fc4da8c7c5958
gdrakoulakos <georgediopan@gmail.com>
2024-01-13 01:32:39 +0200

    Home Page: The names of the categories were changed to plural

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -7,10 +7,10 @@ function Header () {
                 <a href="http://localhost:3000"><img src="./images/musicCornerLogo.jpg" alt="logo of music corner" /></a>
             </div>
             <div className={styles.headerMenu}>
-                <a href="http://localhost:3000">String</a>
-                <a href="http://localhost:3000">Percussion</a>
-                <a href="http://localhost:3000">Brass</a>
-                <a href="http://localhost:3000">Woodwind</a>
+                <a href="http://localhost:3000">Strings</a>
+                <a href="http://localhost:3000">Percussions</a>
+                <a href="http://localhost:3000">Brasses</a>
+                <a href="http://localhost:3000">Woodwinds</a>
                 <a href="http://localhost:3000">Plucked</a>
             </div>
             <div className={styles.headerQuizzes}>
