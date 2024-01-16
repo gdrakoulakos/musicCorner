f5fc43938da72e0dadc52803dacae5e0632facc3
gdrakoulakos <georgediopan@gmail.com>
2024-01-16 02:40:57 +0200

    Changed the 'YoutubeURL' for each string instrument to make the videos more child-friendly

--- src/instruments.js
+++ src/instruments.js
@@ -6,7 +6,7 @@ const instruments = [
       imgURL: "./images/instruments/bass1.jpg",
       audio: "/audio/instruments/bass/bass.mp3",
       description: "The double bass, also known as the contrabass, is the largest and lowest-pitched member of the string instrument family. It typically has four strings tuned in perfect fourths. Played with a bow or plucked, it provides the foundational bass line in classical, jazz, and various other music genres. With a deep, resonant sound, the double bass serves as both a melodic and rhythmic anchor in orchestras and ensembles. Its size and shape make it stand upright on the floor, requiring the musician to either sit or stand while playing.",
-      youtubeURL:"https://www.youtube.com/watch?v=nUUVSxZ4ohI&list=PLqR22EoucCyccs5J639SCefaM7mD9dMSz&index=10"
+      youtubeURL:"https://www.youtube.com/watch?v=34ymJ2_EIbo&list=PLr4S3PqZjXgm5GVG16vHWev7j6vOSrZVW&index=15"
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ const instruments = [
         imgURL: "./images/instruments/cello1.jpg",
         audio: "/audio/instruments/cello/cello.mp3",
         description: "The cello, a versatile bowed string instrument, is slightly smaller than the double bass and larger than the violin and viola. It has four strings tuned in perfect fifths and is played while seated, supported between the knees. The cello produces a rich and warm tone, making it suitable for solo performances, chamber music, and orchestral settings. Renowned for its expressive capabilities, the cello often takes on both melodic and supportive roles in various musical genres, showcasing its dynamic range and emotive qualities.",
-        youtubeURL:"https://www.youtube.com/watch?v=RcqzPoMza7c&list=PLqR22EoucCyccs5J639SCefaM7mD9dMSz&index=11"
+        youtubeURL:"https://www.youtube.com/watch?v=VymNSPOhquY"
       },
       {
         id: 3,
@@ -24,7 +24,7 @@ const instruments = [
         imgURL: "./images/instruments/bass1.jpg",
         audio: "/audio/instruments/viola/viola.mp3",
         description: "The viola is a bowed string instrument slightly larger than the violin and smaller than the cello. It has four strings tuned in perfect fifths, with a deeper and richer tone compared to the violin. Viola players, known as violists, typically sit while playing, supporting the instrument on their shoulder. The viola serves a vital role in orchestras and chamber music, providing a middle voice that bridges the gap between the higher-pitched violins and the lower-pitched cellos and basses. Its expressive capabilities contribute to a wide range of musical genres, showcasing its versatility in both classical and contemporary settings.",
-        youtubeURL:"https://www.youtube.com/watch?v=XierDLeUiYg&list=PLqR22EoucCyccs5J639SCefaM7mD9dMSz&index=2"
+        youtubeURL:"https://www.youtube.com/watch?v=aAWi4XyUCsI"
       },
       {
         id: 4,
@@ -33,7 +33,7 @@ const instruments = [
         imgURL: "./images/instruments/violin1.jpg",
         audio: "/audio/instruments/violin/violin.mp3",
         description: "The violin is a small, high-pitched, and versatile bowed string instrument with four strings tuned in perfect fifths. Played with a bow or plucked, it is a central member of classical orchestras, chamber ensembles, and various music genres. Violinists, who play while holding the instrument under their chin, produce a bright and expressive sound. The violin's agility and dynamic range make it suitable for both solo performances and ensemble playing. Its iconic shape and role in diverse musical styles contribute to its prominence as one of the most widely recognized and played instruments globally.",
-        youtubeURL:"https://www.youtube.com/watch?v=zgaQFLUdUL0&list=PLqR22EoucCyccs5J639SCefaM7mD9dMSz&index=12"
+        youtubeURL:"https://www.youtube.com/watch?v=LBeLX4-24Us&list=PLr4S3PqZjXgm5GVG16vHWev7j6vOSrZVW&index=18"
       }
   ];
   
