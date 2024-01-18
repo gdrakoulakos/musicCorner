ceb6acd5c7df866045d84da92c35ad53ef11bc41
gdrakoulakos <georgediopan@gmail.com>
2024-01-18 18:50:02 +0200

    Updated CSS styling for answer's divs

--- components/question/index.jsx
+++ components/question/index.jsx
@@ -5,10 +5,10 @@ function Question (props) {
       <><h3>{props.question}</h3>
         <img src={props.imagePath} alt={props.question}></img>
         <div className={styles.answersContainer}>
-            <div>{props.answer1}</div>
-            <div>{props.answer2}</div>
-            <div>{props.answer3}</div>
-            <div>{props.answer4}</div>
+            <p>{props.answer1}</p>
+            <p>{props.answer2}</p>
+            <p>{props.answer3}</p>
+            <p>{props.answer4}</p>
         </div>
         </>
     )
